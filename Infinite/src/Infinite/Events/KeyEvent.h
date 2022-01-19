49ec13bae716bb282a7e351eea9d825960bbb61f
Yousazoe <961652168@qq.com>
2022-01-20 01:24:34 +0800

    Added events to ImGui layer

--- Infinite/src/Infinite/Events/KeyEvent.h
+++ Infinite/src/Infinite/Events/KeyEvent.h
@@ -52,4 +52,20 @@ namespace Infinite {
 
 		EVENT_CLASS_TYPE(KeyReleased)
 	};
+
+	class INFINITE_API KeyTypedEvent : public KeyEvent
+	{
+	public:
+		KeyTypedEvent(int keycode)
+			: KeyEvent(keycode) {}
+
+		std::string ToString() const override
+		{
+			std::stringstream ss;
+			ss << "KeyTypedEvent: " << m_KeyCode;
+			return ss.str();
+		}
+
+		EVENT_CLASS_TYPE(KeyTyped)
+	};
 }
--- Infinite/src/Infinite/ImGui/ImGuiLayer.h
+++ Infinite/src/Infinite/ImGui/ImGuiLayer.h
@@ -2,6 +2,10 @@
 
 #include "../Layer.h"
 
+#include "../Events/KeyEvent.h"
+#include "../Events/MouseEvent.h"
+#include "../Events/ApplicationEvent.h"
+
 namespace Infinite {
 
 	class INFINITE_API ImGuiLayer : public Layer
@@ -14,6 +18,16 @@ namespace Infinite {
 		void OnDetach();
 		void OnUpdate();
 		void OnEvent(Event& event);
+	private:
+		bool OnMouseButtonPressedEvent(MouseButtonPressedEvent& e);
+		bool OnMouseButtonReleasedEvent(MouseButtonReleasedEvent& e);
+		bool OnMouseMovedEvent(MouseMovedEvent& e);
+		bool OnMouseScrolledEvent(MouseScrolledEvent& e);
+		bool OnKeyPressedEvent(KeyPressedEvent& e);
+		bool OnKeyReleasedEvent(KeyReleasedEvent& e);
+		bool OnKeyTypedEvent(KeyTypedEvent& e);
+		bool OnWindowResizeEvent(WindowResizeEvent& e);
+
 	private:
 		float m_Time = 0.0f;
 
