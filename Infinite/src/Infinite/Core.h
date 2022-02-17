9de548ce35cc9a5c7b3ee245c597d92860ac5cb3
Yousazoe <961652168@qq.com>
2022-02-17 10:38:25 +0800

    Added Infinite::Scope and Infinite::Ref definitions

--- Infinite/src/Infinite/Core.h
+++ Infinite/src/Infinite/Core.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <memory>
+
 #ifdef IFN_PLATFORM_WINDOWS
 #if IFN_DYNAMIC_LINK
 	#ifdef IFN_BUILD_DLL
@@ -29,3 +31,13 @@
 #define BIT(x) (1 << (x))
 
 #define IFN_BIND_EVENT_FN(fn) std::bind(&fn, this, std::placeholders::_1)
+
+namespace Infinite {
+
+	template<typename T>
+	using Scope = std::unique_ptr<T>;
+
+	template<typename T>
+	using Ref = std::shared_ptr<T>;
+
+}
