5d93e3464eade8a352e7414ea8b2181c10aebdd2
Yousazoe <961652168@qq.com>
2022-02-18 12:33:22 +0800

    Fixed a bug in LayerStack which would occur if passing an overlay or

--- Infinite/src/Infinite/LayerStack.cpp
+++ Infinite/src/Infinite/LayerStack.cpp
@@ -30,7 +30,7 @@ namespace Infinite {
 	void LayerStack::PopLayer(Layer* layer)
 	{
 		auto it = std::find(m_Layers.begin(),m_Layers.end(),layer);
-		if (it != m_Layers.end())
+		if (it != m_Layers.begin() + m_LayerInsertIndex)
 		{
 			layer->OnDetach();
 			m_Layers.erase(it);
