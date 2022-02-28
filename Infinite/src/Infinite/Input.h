fefc10c0d6b39c08ec5b34ec5553bd87c5a78d54
Yousazoe <961652168@qq.com>
2022-02-28 16:32:47 +0800

    Made Input a true singleton

--- Infinite/src/Infinite/Input.h
+++ Infinite/src/Infinite/Input.h
@@ -6,7 +6,12 @@ namespace Infinite {
 
 	class INFINITE_API Input
 	{
+	protected:
+		Input() = default;
 	public:
+		Input(const Input&) = delete;
+		Input& operator=(const Input&) = delete;
+
 		inline static bool IsKeyPressed(int keycode) { return s_Instance->IsKeyPressedImpl(keycode); }
 
 		inline static bool IsMouseButtonPressed(int button) { return s_Instance->IsMouseButtonPressedImpl(button); }
