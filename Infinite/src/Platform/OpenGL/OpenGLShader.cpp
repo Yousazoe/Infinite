7923cd913c27bb8ed9c9c54824c4b4357c440c3d
Yousazoe <961652168@qq.com>
2022-03-25 20:57:52 +0800

    Added file reading check

--- Infinite/src/Platform/OpenGL/OpenGLShader.cpp
+++ Infinite/src/Platform/OpenGL/OpenGLShader.cpp
@@ -54,10 +54,18 @@ namespace Infinite {
 		if (in)
 		{
 			in.seekg(0, std::ios::end);
-			result.resize(in.tellg());
-			in.seekg(0, std::ios::beg);
-			in.read(&result[0], result.size());
-			in.close();
+			size_t size = in.tellg();
+			if (size != -1)
+			{
+				result.resize(size);
+				in.seekg(0, std::ios::beg);
+				in.read(&result[0], size);
+				in.close();
+			}
+			else
+			{
+				IFN_CORE_ERROR("Could not read from file '{0}'", filepath);
+			}
 		}
 		else
 		{
