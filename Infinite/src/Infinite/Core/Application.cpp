673031a481aa507a25c5fe403550135a3fad2f55
Yousazoe <961652168@qq.com>
2022-04-08 20:00:37 +0800

    Missing iterator wrappers in LayerStack.h

--- Infinite/src/Infinite/Core/Application.cpp
+++ Infinite/src/Infinite/Core/Application.cpp
@@ -62,9 +62,9 @@ namespace Infinite {
 		dispatcher.Dispatch<WindowCloseEvent>(IFN_BIND_EVENT_FN(Application::OnWindowClose));
 		dispatcher.Dispatch<WindowResizeEvent>(IFN_BIND_EVENT_FN(Application::OnWindowResize));
 
-		for (auto it = m_LayerStack.end(); it != m_LayerStack.begin();)
+		for (auto it = m_LayerStack.rbegin(); it != m_LayerStack.rend(); ++it)
 		{
-			(*--it)->OnEvent(e);
+			(*it)->OnEvent(e);
 			if (e.Handled)
 				break;
 		}
